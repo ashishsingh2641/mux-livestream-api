9e0a1d0f2d472e4cdf5aa2ec08b4229451c6e75d
Ashish Singh <ashish.singh@t-systems.com>
2025-11-04 15:52:19 +0530

    added crert

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,49 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import * as fs from 'fs';
+import { Logger } from '@nestjs/common';
+
+dotenv.config({ path: './.env' });
+const logger = new Logger('Bootstrap');
 
 async function bootstrap() {
-  dotenv.config({ path: './.env' });
-
-  const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
+  const httpsPort = process.env.HTTPS_PORT;
+  
+  const keyPath = process.env.SSL_KEY_PATH;
+  const certPath = process.env.SSL_CERT_PATH;
+  
+  let app;
+  let protocol = 'http';
+  
+  if (keyPath && certPath && fs.existsSync(keyPath) && fs.existsSync(certPath)) {
+    // HTTPS Configuration
+    const httpsOptions = {
+      key: fs.readFileSync(keyPath),
+      cert: fs.readFileSync(certPath),
+    };
+    app = await NestFactory.create(AppModule, {
+      httpsOptions,
+    });
+    protocol = 'https';
+    logger.log('Starting server with HTTPS...');
+    app.enableCors();
 
-  app.enableCors();
+  await app.listen(httpsPort);
+  console.log(`🚀 Server running on ${protocol}://localhost:${port}`);
 
-  await app.listen(port);
-  console.log(`🚀 Server running on http://localhost:${port}`);
+  } else {
+    // HTTP Configuration (Default)
+    app = await NestFactory.create(AppModule);
+    logger.log('Starting server with HTTP...');
+     app.enableCors();
+
+  await app.listen(httpsPort);
+  console.log(`🚀 Server running on ${protocol}://localhost:${port}`);
+  }
+
+
 }
 
 bootstrap();
